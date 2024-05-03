a82da73e6ed08157433c29558c5124445d6f9407
farmeh <farmeh@utu.fi>
2024-05-03 15:34:26 +0300

    add original model to download script for Large-scale execution

--- LargeScaleRelationExtractionPipeline/get_model.sh
+++ LargeScaleRelationExtractionPipeline/get_model.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p original_model
 mkdir -p the_best_model
 mkdir -p output
 mkdir -p jobs
@@ -6,6 +7,11 @@ mkdir -p logs
 mkdir -p preds
 mkdir -p puhtilogs
 
+wget 'https://dl.fbaipublicfiles.com/biolm/RoBERTa-large-PM-M3-Voc-hf.tar.gz' -O model.tar.gz
+tar -xvf model.tar.gz -C ./original_model
+rm model.tar.gz
+
 wget 'https://zenodo.org/records/8139717/files/relation_extraction_string_v12_best_model.tar.gz?download=1' -O model.tar.gz
 tar -xvf model.tar.gz -C ./the_best_model
 rm model.tar.gz
+
